b7457cb8a02d49f2e28d3a6c8984b609a2740714
sunghoon <shun1423@naver.com>
2023-12-28 16:09:55 +0900

    test2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <h1 className="text-3xl font-bold underline">Dental Project!</h1>
+  return <h1 className="text-3xl font-bold underline">Dental Project Test</h1>
 }
