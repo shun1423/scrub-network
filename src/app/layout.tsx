885a1f9e1283599917b6c869747748273c450150
sunghoon <shun1423@naver.com>
2023-12-31 02:21:15 +0900

    framer-motion install

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import GNB from '@/components/layouts/gnb'
+import Footer from '@/components/layouts/footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +18,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <GNB />
       <body className={inter.className}>{children}</body>
+      <Footer />
     </html>
   )
 }
