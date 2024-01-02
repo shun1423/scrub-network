c92312436daae6efcae176f676396c1533700b46
sunghoon <shun1423@naver.com>
2024-01-02 15:08:04 +0900

    default sparkles core implement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import TypewriterEffectSmoothDemo from '@/components/layouts/typewriterEffect'
 import GNB from '@/components/layouts/gnb'
 import TracingBeamDemo from '@/components/layouts/tracingBeam'
+import SparklesCore from '@/components/ui/sparkles'
+import SparklesPreview from '@/components/layouts/sparklesCore'
 
 export default function Home() {
   return (
     <div>
       {' '}
       <GNB />
+      <SparklesPreview />
       <TypewriterEffectSmoothDemo />
       <TracingBeamDemo />
     </div>
